d9968c8621bbddda96fcc84fdc1c8d70b9727290
Ajay Lohar <ajaylohar4040@gmail.com>
2026-05-16 16:46:07 +0530

    Add files via upload
    switch & case basic example in js

---
+++ switch&case.js
@@ -0,0 +1,15 @@
+
+//switch statmant 
+let color = "green";
+
+switch (color) {
+    case "red":
+        console.log("stoop red light is here ");
+        break;
+    case "yellow":
+        console.log("start your vehical and stop here ");
+        break;
+    case "green":
+        console.log("go fast ");
+        break;
+}
